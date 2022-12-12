df49573a35b5258a948ca6a25787a3954f329a70
Thoufeek <thoufeekrahuman9361@gmail.com>
2022-12-12 18:55:11 +0530

    commited

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const boxes = document.getElementsByClassName("gradient");
+const reset = document.getElementsByClassName('btn');
 console.log(boxes.item(0));
 let isSecondClick = false;
-let player_X_points = 1;
-let player_Y_points = 3;
+let player_X_points = 0;
+let player_Y_points = 0;
 
 boxesArr = [...boxes];
 console.log(boxesArr);
 console.log(typeof boxesArr);
 
+function isPlayerWon(element) {
+
+  if(player_X_points === 3) {
+  }
+}
 
 
 boxesArr.forEach((element) => {
@@ -19,6 +25,7 @@ boxesArr.forEach((element) => {
     if (!isSecondClick) {
       element.innerText = "X";
       element.classList.add("bg-danger", "gradient-applier");
+      player_X_points += 1;
       isSecondClick = true;
       isPlayerWon(element);
     } else {
@@ -28,6 +35,8 @@ boxesArr.forEach((element) => {
     }
   });
 });
+
+reset.add
 
 // boxes.item(i).innerText = "X"
 //         boxes.item(i).classList.remove("bg-light")
