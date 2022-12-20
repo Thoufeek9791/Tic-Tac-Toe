de01273694392e34bc7db7b0b980c48d5414446e
Thoufeek <thoufeekrahuman9361@gmail.com>
2022-12-20 11:24:25 +0530

    commited

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const boxes = document.getElementsByClassName("gradient");
 const reset = document.getElementsByClassName("btn");
+const win = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
 console.log(boxes.item(0));
 let isSecondClick = false;
 let player_X_points = 0;
@@ -9,32 +19,40 @@ boxesArr = [...boxes];
 console.log(boxesArr);
 console.log(typeof boxesArr);
 
-function isPlayerWon(element) {
-  if (player_X_points === 3) {
+function isPlayerXWon(element) {
+  let playerX = element.getAttribute('data-index');
+  if(playerX === 0) {
+    
   }
+}
+
+function isPlayerYWon(element) {
+  let playerY = element.getAttribute('data-index');
+  console.log("player y attribute", playerY);
 }
 
 boxesArr.forEach((element) => {
   console.log(element);
   element.addEventListener("click", () => {
     console.log(element);
-    element.classList.remove("bg-light");
+    element.classList.remove("bg-light", "bg-dark");
     element.classList.add("pe-none");
     if (!isSecondClick) {
       element.innerText = "X";
       element.classList.add("bg-danger", "gradient-applier");
       player_X_points += 1;
       isSecondClick = true;
-      isPlayerWon(element);
+      isPlayerXWon(element);
     } else {
       element.innerText = "O";
       element.classList.add("bg-success", "gradient-applier");
       isSecondClick = false;
+      isPlayerYWon(element)
     }
   });
 });
 
-reset.add;
+
 
 // boxes.item(i).innerText = "X"
 //         boxes.item(i).classList.remove("bg-light")
