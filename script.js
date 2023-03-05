8d575aa69c39cbce99680809375c11ea764012f0
Thoufeek <thoufeekrahuman9361@gmail.com>
2023-03-05 20:22:24 +0530

    added functionality

--- script.js
+++ script.js
@@ -1,59 +1,76 @@
 const boxes = document.getElementsByClassName("gradient");
-const reset = document.getElementsByClassName("btn");
-const win = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-]
-console.log(boxes.item(0));
+const modal = document.getElementById("WonAlertModal");
 let isSecondClick = false;
-let player_X_points = 0;
-let player_Y_points = 0;
-
+const win = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 boxesArr = [...boxes];
-console.log(boxesArr);
-console.log(typeof boxesArr);
 
-function isPlayerXWon(element) {
-  let playerX = element.getAttribute('data-index');
-  if(playerX === 0) {
-    
-  }
+function isPlayerWon(element) {
+  let adjustmentElement = [];
+  let player = parseInt(element.getAttribute("data-index"));
+  console.log(typeof player);
+  win.forEach((outcomes) => {
+    if (outcomes.includes(player)) {
+      adjustmentElement.push(outcomes);
+    }
+  });
+  adjustmentElement.forEach((myElement) => {
+    const isGameWon = myElement.every(
+      (index) =>
+        element.innerText ===
+        document.getElementById(`value-${index + 1}`).innerText
+    );
+    if (isGameWon) {
+      $(document).ready(function () {
+        const winner = $(".winner");
+        $(".gradient").addClass("pe-none");
+        element.innerText === "X"
+          ? winner.addClass("bg-danger")
+          : winner.addClass("bg-success");
+        winner.text(element.innerText);
+        $("#WonAlertModal").modal("show");
+        $(".gradient").text(" ");
+        $(".gradient").removeClass("gradient-applier bg-danger bg-success");
+        $(".gradient").addClass("bg-light");
+      });
+      return;
+    }
+  });
 }
 
-function isPlayerYWon(element) {
-  let playerY = element.getAttribute('data-index');
-  console.log("player y attribute", playerY);
+function reset() {
+  $(document).ready(function () {
+    $(".gradient").removeClass("pe-none");
+  });
 }
+
 
 boxesArr.forEach((element) => {
   console.log(element);
   element.addEventListener("click", () => {
     console.log(element);
-    element.classList.remove("bg-light", "bg-dark");
+    element.classList.remove("bg-light");
     element.classList.add("pe-none");
     if (!isSecondClick) {
       element.innerText = "X";
       element.classList.add("bg-danger", "gradient-applier");
-      player_X_points += 1;
       isSecondClick = true;
-      isPlayerXWon(element);
+      isPlayerWon(element);
+      reset();
     } else {
       element.innerText = "O";
       element.classList.add("bg-success", "gradient-applier");
       isSecondClick = false;
-      isPlayerYWon(element)
+      isPlayerWon(element);
+      reset();
     }
   });
 });
-
-
-
-// boxes.item(i).innerText = "X"
-//         boxes.item(i).classList.remove("bg-light")
-//         boxes.item(i).classList.add("bg-danger","gradient-applier")
